c68b90d0e5137858f2f147386013293c593f0469
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2021-03-09 09:14:28 -0600

    connected

--- server/app.js
+++ server/app.js
@@ -1,5 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const db_url = process.env.DB_URL;
+
+mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+mongoose.connection.once("open", () => {
+    console.log("Yes We are connected");
+});
 
 const schema = require("./schema/schema");
 
